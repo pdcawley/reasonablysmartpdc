79d1fb1c213fa14c4688f17cb69ab67b5d91d8cf
Piers Cawley <pdcawley@bofh.org.uk>
2008-07-24 10:35:14 +0100

    Got Theory's Test.More working

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,4 +1,20 @@
-function main() {
-  var file = system.filesystem.get('index.html');
-  return [ 200, "Ok", ["Content-Type", "text/html" ], file ];
+system.use("lib.Test.Builder");
+system.use("lib.Test.More");
+
+function debug(msg) {
+  if (debug.debugging) {
+    system.console.log(msg);
+  }
+}
+
+debug.debugging = true;
+
+function main () {
+  plan({tests: 1});
+
+  ok(1);
+
+  debug("Buffer is: '" + Test.More.builder().buffer + "'");
+
+  return [200, "Ok", ["Content-Type", "text/plain"], Test.More.builder().buffer];
 }
