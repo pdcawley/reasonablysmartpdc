2c6d86425a3d964ef779c7540e1c1cd6dea3bb95
Piers Cawley <pdcawley@bofh.org.uk>
2008-07-25 13:47:11 +0100

    Bootstrap is getting a wee bit more intelligent. Not _very_ intelligent though...

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -12,14 +12,44 @@ function debug(msg) {
 
 debug.debugging = true;
 
+const NOT_FOUND = [404, "Not found", ["Content-Type", "text/html"], "<html><head><title>404 Not Found</title></head><body><p>Nope, I dunno where that's gone</p></body></html>"];
+
 function main () {
-  diag("First tests");
-  plan({tests: 1});
+  var uri = system.request.uri
+  , path = uri.split(/\//)
+  , target = app
+  , [action, format] = path.pop().split(/\.(?=[^.]*$)/)
+  , item // for that for loop 'cos let doesn't work
+  ;
 
-  ok(1);
+  system.console.log(action);
+  debug(action);
+  debug('[' + path.join(', ') + ']');
 
-  Test.More.builder().reset();
-  plan({tests: 1});
-  ok(1);
-  return [200, "Ok", ["Content-Type", "text/plain"], Test.More.builder().buffer];
+  for each(item in path) {
+    if (item) {
+      debug("Checking " + item);
+      target = target[item];
+      if (!target) {
+        return NOT_FOUND;
+      }
+
+    }
+  }
+
+  switch (action) {
+  case 'test':
+    try {
+      target.runTests();
+      return [200, "Ok", ["Content-Type", "text/plain"], Test.More.builder().buffer];
+    }
+    catch (e) {
+      return [ 500, "Internal Server Error"
+             , ["Content-Type", "text/plain"]
+             , "Got error: " + e + "\n\n" + Test.More.builder().buffer];
+    }
+    break;
+  default:
+    return NOT_FOUND;
+  }
 }
